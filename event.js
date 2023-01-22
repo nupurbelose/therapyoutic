9a6c756cb7c0a44fc67eeeee057fe14cd7a1a11b
Nupur Belose <86060004+nupurbelose@users.noreply.github.com>
2023-01-23 00:06:20 +0530

    Update event.js

--- event.js
+++ event.js
@@ -12,9 +12,28 @@ jQuery(document).ready(function( $ ) {
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
     return false;
   });
-  
 
- 
+  // Header fixed on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Real view height for mobile devices
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    $('#intro').css({ height: $(window).height() });
+  }
+
+  // Initiate the wowjs animation library
+  new WOW().init();
+
   // Initialize Venobox
   $('.venobox').venobox({
     bgcolor: '',
